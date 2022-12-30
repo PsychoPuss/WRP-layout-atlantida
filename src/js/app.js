44267e1776097ac076a2f815a0d3d63b92217bc6
PsychoPuss <evgeniy.v.ilyin@gmail.com>
2022-12-31 01:46:07 +0300

    cookies panel and fixes v2

--- src/js/app.js
+++ src/js/app.js
@@ -10,5 +10,6 @@ import "./modules/googlemap.js";
 import "./modules/swiper.js";
 import "./modules/isotope_fancybox.js";
 import "./modules/nouislider.js";
+import "./modules/cookies.js";
 import "../vendors/select-styler/LC-select-main/lc_select.min.js";
 
